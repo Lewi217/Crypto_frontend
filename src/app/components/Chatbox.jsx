cb10c4a279939b53a1245c4ff484ab5098f01258
wanjohi lewis <wanjohilewi7@gmail.com>
2024-11-03 19:09:24 +0300

    created chatbox

---
+++ src/app/components/Chatbox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chatbox = () => {
+  return (
+    <div className='chatbox blur-background large-shadow z-50 bg-[#000518]
+    bg-opacity-70 w-[90vw] md:w[]'>
+      
+    </div>
+  )
+}
+
+export default Chatbox
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Chatbox from "./components/Chatbox";
 
 export default function Home() {
   return (
 
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
         
+        <Chatbox/>
+
       </main>
       
   );
-}
+} 
