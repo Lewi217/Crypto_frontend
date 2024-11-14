01e859218aa2f322dd3faa854297e4892eed7618
wanjohi lewis <wanjohilewi7@gmail.com>
2024-11-15 00:55:45 +0300

    made chages to chatbox

--- src/app/components/Chatbox.jsx
+++ src/app/components/Chatbox.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 
 const Chatbox = () => {
+  const [responses, setResponses] = useState([]);
+  const fetchCoinDetails = async (prompt) => {
+    setLoading(true);
+  }
   return (
     <div className='chatbox blur-background large-shadow z-50 bg-[#000518]
     bg-opacity-70 w-[90vw] md:w[]'>
-    // this is  crypto frontend project
+      <div className="h-[13%] pl-3 border-b border-gray-700 flex gap-x-4 items-center">
+        <img
+          className="rounded-full w-12 h-12"
+          src="https://cdn.pixabay.com/photo/2023/05/29/18/53/cyborg-8026949_640.jpg"
+        />
+
+        <div>
+          <h1 className="text-lg font-semibold">Ai Chat Bot</h1>
+          <p className="text-gray-400 text-sm">
+            Real Time Crypto Market Data
+          </p>
+        </div>
+      </div>
+
+      <div className="h-[77%]">
+
+        <div className="p-10 gap-5 h-full flex flex-col justify-center items-center">
+          <p className=" font-bold text-2xl">
+            Welcome to the Crypto Chat Bot
+          </p>
+          <p className="text-gray-500">inquire about market data.</p>
+        </div>
+
+      </div>
+      <div className="h-[10%] bottom-0 px-5">
+      <input
+            onKeyPress={(e) => {
+              if (e.key == "Enter") {
+                setResponses([...responses, { message: prompt, role: "user" }]);
+                fetchCoinDetails(e.target.value);
+                setPrompt("");
+              }
+            }}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="h-full rounded-full  border-gray-700 border bg-transparent px-5 w-full outline-none"
+            placeholder="give your prompt"
+          />
+      </div>
+
     </div>
   )
 }
